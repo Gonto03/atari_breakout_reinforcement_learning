8a566b10c998490fc8e294314e6e515813d32e2c
Gonto03 <gapmd2003@gmail.com>
2023-12-10 15:07:09 +0000

    template

--- testing_breakout.py
+++
@@ -1,5 +0,0 @@
-import gymnasium as gym
-import time
-import matplotlib.pyplot as plt
-
-env = gym.make("ALE/Breakout-v5", render_mode = "human", full_action_space=False, repeat_action_probability=0.1, obs_type="rgb")
