b7debfd4b6c0fbd51f727ce5982407cad6dc13de
Gonto03 <gapmd2003@gmail.com>
2023-12-12 16:55:52 +0000

    get_broken_block_coordinates

--- customizing_environment.py
+++ customizing_environment.py
@@ -13,16 +13,23 @@ import random
 import stable_baselines3
 from stable_baselines3 import DQN
 
+def get_broken_block_coordinates(prev_obs, cur_obs):
+    for i in range():
+        for j in range():
+            aux = [True if prev_obs[i][j][l]==cur_obs[i][j][l] else False for l in range(3)]
+            ######################################################
+
 class CustomRewardBreakout(Wrapper):
     def __init__(self, env):
         super(CustomRewardBreakout, self).__init__(env)
 
-    def step(self, n_lives, action):
+    def step(self, prev_obs, n_lives, action):
         # Perform the action in the original environment
         observation, reward, done, _, info = self.env.step(action)
         if info['lives'] != n_lives:
             reward -= 10        # reward de -10 atribuída ao agente quando este perde uma vida
         if reward > 0:          # se algum bloco foi partido, verificar quais as coordenadas deste
+            _ = get_broken_block_coordinates(prev_obs, observation)
             pass################################
 
         return observation, reward, done, info
@@ -47,9 +54,8 @@ def testing():
         n_lives = info['lives']
         
         while not terminated:
-            action, _states = model.predict(observation, deterministic=True)
-            # action = random.choice([0,2,3])
-            observation, reward, terminated, info = env.step(n_lives, action)
+            action, _states = model.predict(observation, n_lives, deterministic=True)
+            observation, reward, terminated, info = env.step(observation, action)
             score += reward
             if info['lives'] == 0:
                 break
